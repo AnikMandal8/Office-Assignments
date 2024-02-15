5da5ed869cf7e718e4af4e3efec5deaf01d7fb20
MANDAL <anik.a.mandal@capgemini.com>
2024-02-16 04:29:56 +0530

    sidenav add

--- POC_1/poc.js
+++ POC_1/poc.js
@@ -1,3 +1,35 @@
-function trial(){
+function trial() {
     console.log("hello")
+}
+function modeChange(val) {
+    console.log("jkhjfhs", val)
+    if (val == "night") {
+        document.querySelector("body").style.backgroundColor = '#141414';
+        document.querySelector(".html_info").style.color = "white";
+        document.querySelector(".css_info").style.color = "white";
+        document.querySelector(".js_info").style.color = "white";
+        document.querySelector(".react_info").style.color = "white";
+        document.querySelector("#nightmode").style.display = "none";
+        document.querySelector("#daymode").style.display = "inline";
+    }
+    else {
+        document.querySelector("body").style.backgroundColor = 'white';
+        document.querySelector(".html_info").style.color = "black";
+        document.querySelector(".css_info").style.color = "black";
+        document.querySelector(".js_info").style.color = "black";
+        document.querySelector(".react_info").style.color = "black";
+        document.querySelector("#nightmode").style.display = "inline";
+        document.querySelector("#daymode").style.display = "none";
+    }
+}
+let toggle=false;
+function openSidenav(){
+    toggle=!toggle;
+    if(toggle){
+    document.querySelector(".sidenav").style.display="inline";
+    }
+    else{
+        document.querySelector(".sidenav").style.display="none";
+
+    }
 }
